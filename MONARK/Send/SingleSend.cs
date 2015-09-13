c5cbcd2ecfc7742abab0f55b93fcb0ec2deab596
dentych <dennis@tychsen.me>
2015-09-13 23:59:25 +0200

    ISend Interface + SingleSend implementation

---
+++ MONARK/Send/SingleSend.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace MONARK
+{
+    internal class SingleSend : ISend
+    {
+        private IAPI api;
+
+        public SingleSend(IAPI api)
+        {
+            this.api = api;
+        }
+
+        public bool Send(string receiver, string sender, string msg)
+        {
+            var apiUrl = api.GenerateUrl(receiver, sender, msg);
+            var http = new HttpRequest();
+            string ret = http.GrabData(apiUrl);
+
+            if (ret != "0")
+            {
+                MessageBox.Show("Server returned: " + ret);
+            }
+
+            return true;
+        }
+    }
+}
