48d0dbb120fa15f2e30b27b37cc5be3a60455d40
Benjamin Larsem <benlarsendk@gmail.com>
2015-09-13 23:50:43 +0200

    Added IAPI

---
+++ MONARK/API.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MONARK
+{
+    public interface IAPI
+    {
+        string GenerateUrl(string rec, string send, string msg);
+    }
+
+
+    public class SMSIT : IAPI
+    {
+        private string APIKey;
+
+        public SMSIT(string API)
+        {
+            APIKey = API;
+        }
+
+        public string GenerateUrl(string rec, string send, string msg)
+        {
+            return "http://www.smsit.dk/api/sendSms.php?apiKey=" + APIKey + "&charset=UTF-8&senderId=" + send +
+                       "&mobile=45" + rec + "&message=" + msg;
+        }
+    }
+}
