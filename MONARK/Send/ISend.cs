8c563024012cb1677567ad91ad8e5f2f29a34cf8
dentych <dennis@tychsen.me>
2015-09-13 23:46:31 +0200

    Interface for Send classes

---
+++ MONARK/Send/ISend.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MONARK
+{
+    interface ISend
+    {
+        /*!
+        Send msg to receiver as receiver.
+        */
+        bool Send(string reciever, string sender, string msg);
+    }
+}
